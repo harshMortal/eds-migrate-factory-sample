8a07f2ed0fd4f11bd59d19d66522cd4a03d08c3b
harshMortal <73352117+harshMortal@users.noreply.github.com>
2025-11-06 09:36:32 +0530

    Create hero block JS

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,4 +1,6 @@
 export default function decorate(block) {
   const bgImage = block.dataset.backgroundImage;
-  if (bgImage) block.style.backgroundImage = `url(${bgImage})`;
+  if (bgImage) {
+    block.style.backgroundImage = `url(${bgImage})`;
+  }
 }
